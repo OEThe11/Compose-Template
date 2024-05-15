a0d107570fd5dd26003d9d1157c442b8d0b004ee
Osayuki's Work <oethe68@gmail.com>
2024-05-14 22:11:06 -0400

    Moved the list down a bit.

--- app/src/main/java/com/example/composetemplate/screens/main/MainScreen.kt
+++ app/src/main/java/com/example/composetemplate/screens/main/MainScreen.kt
@@ -3,6 +3,7 @@ package com.example.composetemplate.screens.main
 import android.annotation.SuppressLint
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
@@ -14,6 +15,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.example.composetemplate.data.Resource
@@ -35,7 +37,8 @@ fun MainScreen(navController: NavController, viewModel: MainViewModel = hiltView
         bottomBar = {}
     ) {
         Surface(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier
+                .fillMaxSize(),
             color = Color.DarkGray
         ) {
           when(standardItem){
@@ -56,7 +59,7 @@ fun MainScreen(navController: NavController, viewModel: MainViewModel = hiltView
 
 @Composable
 fun MainList(list: Resource<List<StandardEntity>>, navController: NavController){
-    LazyColumn{
+    LazyColumn(modifier = Modifier.padding(top = 30.dp)){
         items(list.data!!.size){item ->
             StandardCard(
                 standardItem = list.data[item],
